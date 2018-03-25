1a8c43f8af79a4f7328e8ed3939b3b0addac3b4f
Siddharth Sharma <sharma88@buffalo.edu>
2018-03-24 23:37:51 -0400

    added code for getiing current location

--- client/src/components/Weather.vue
+++ client/src/components/Weather.vue
@@ -1,5 +1,5 @@
 <template>
-<div>
+<div onload="initialize()">
   <h1>Weather</h1>
   <input
   type="text"
@@ -36,49 +36,63 @@ export default {
       Api().get('/zipcode/'+this.zipcode) .then(response => {
       this.weatherDatas = response.data
     })
-      // console.log(typeof JSON.parse(res.data));
-      // console.log(res);
 
-      // this.weatherDatas = res.data 
-      // const datataa = res.data
-      // console.log(JSON.stringify(this.weatherDatas))
-      // console.log(this.weatherDatas);
-      // console.log(dat);
-      // console.log(JSON.parse(res.body));
-
-      // const response = await AuthenticationServices.register({
-        // zipcode: this.zipcode
-      // })
-      // const x = document.getElementById("weatherDetails")
-      // x.innerHTML
-
-      // console.log(response.data);
     },
-    getLocation() {
+    async getLocation() {
+      this.zipcode = this.loadLocation(this)
+       // = currentZipcode
+      // var isComplete = false
+      // loadLocation()
+  //     .then(zipId =>{ 
+  //       console.log('Current Value'+zipId);
+  //   this.zipcode = zipId  
+  // }
+  //     )
+      // }
+      // var currentZipcode = function 
+  // console.log('Current Value'+currentZipcode);
+   // .then(function(){
+    // if(isComplete){
+    
+  // }
+  // })
+    },loadLocation(self){
+        var currentZip = ''
         if (navigator.geolocation) {
           console.log('getting Location')
           navigator.geolocation.getCurrentPosition(function locationFound (position){
-          var head= document.getElementsByTagName('head')[0],script= document.createElement('script');
-          script.src= '//maps.googleapis.com/maps/api/js?sensor=false';
-          head.appendChild(script);
+          // var head= document.getElementsByTagName('head')[0],script= document.createElement('script');
+          // script.src= '//maps.googleapis.com/maps/api/js?sensor=false';
+          // head.appendChild(script);
           // console.log(x);
-          script.onload = function() {
-          // geocoder = new google.maps.Geocoder()
+          // script.onload = function() {
+          // const geoLoc = new google.maps()
+          console.log('Getting location for');
+          // var geocoder = new google.maps.Geocoder()
+          // geocoder  = geoLoc.Geocoder()
           var mylat = position.coords.latitude
           var mylng = position.coords.longitude
-          var latlng = new google.maps.LatLng(mylat, mylng)
+          console.log(this);
+          var latlng = new google.maps.LatLng(mylat, mylng);
           new google.maps.Geocoder().geocode({'latLng': latlng}, function (res, status) {
                     if (status == google.maps.GeocoderStatus.OK && typeof res[0] !== 'undefined') {
                         var zip = res[0].formatted_address.match(/,\s\w{2}\s(\d{5})/);
                         if (zip) {
-                          this.zipcode = zip[1]
+                          console.log(typeof zip[1]);
+                          self.zipcode = '14222'
+                          currentZip = '14211'
+                          console.log(currentZip);
+                          // zip[1]
                           console.log(zip[1]);
+                          // this.getWeatherDetails()
                             // a.value = zip[1];
                         } else fail('Unable to look-up postal code');
                     } else {
                         fail('Unable to look-up geolocation');
                     }
-                })}
+                })
+
+                //}
           // console.log("Lattidue:" + position.address.postalCode)
           // x.innerHTML = "Lattidue:" + position.address.postalCode
         // this.position = position.coords
@@ -87,10 +101,16 @@ export default {
       },function errorHandler(err){
         console.log('Error happened :'+err)
       })
+          // isComplete = true
     } else { 
-        x.innerHTML = "Geolocation is not supported by this browser.";
+      console.log('Geolocation is not supported by this browser.');
+        // x.innerHTML = "Geolocation is not supported by this browser.";
     }
-    }
+    return currentZip
+  },initialize() {
+    var geocoder = new google.maps.Geocoder();
+  }
+
   }
 }
 </script>
